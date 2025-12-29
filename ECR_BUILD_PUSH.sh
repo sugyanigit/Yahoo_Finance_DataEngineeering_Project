9edec5a247402a4fbc0690a7475e5532203ae2b0
Sugyani <sugyani.gcu@gmail.com>
2025-12-28 20:41:03 -0700

    image_id changed

--- ECR_BUILD_PUSH.sh
+++ ECR_BUILD_PUSH.sh
@@ -8,17 +8,17 @@ docker build -t stock-data-ingestion .
 
 ## Command to give the tag Lastest to the Docker Image
 
-docker tag stock-data-ingestion:latest 980921724429.dkr.ecr.us-east-1.amazonaws.com/stock-data-ingestion:latest
-
+docker tag stock-data-ingestion:latest 996818459757.dkr.ecr.us-east-1.amazonaws.com/stock-data-ingestion:latest
+#996818459757.dkr.ecr.us-east-1.amazonaws.com/stock-data-ingestion
 ##you can get above URI from AWS ECR console
 
 ## Command to connect to AWS ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 980921724429.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 996818459757.dkr.ecr.us-east-1.amazonaws.com
 
 ## replace this 980921724429.dkr.ecr.us-east-1.amazonaws.com with your URI from AWS ECR console
 
 ## Command to Push the latest Image to AWS ECR
 
-docker push 980921724429.dkr.ecr.us-east-1.amazonaws.com/stock-data-ingestion:latest
+docker push 996818459757.dkr.ecr.us-east-1.amazonaws.com/stock-data-ingestion:latest
 
 
